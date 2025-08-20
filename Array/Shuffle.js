485bada69265fb366d1dee9e522137d0260b75df
Sanchitjha <sanchitjha8888@gmail.com>
2025-08-20 11:00:27 +0530

    Code rectification

--- Array/Shuffle.js
+++ Array/Shuffle.js
@@ -1,20 +1,13 @@
-//LeetCode 1470
-
-/**
- * @param {number[]} nums
- * @param {number} n
- * @return {number[]}
- */
-var shuffle = function(nums, n) {
+function shuffle(nums, n) {
     let result = [];
     for (let i = 0; i < n; i++) {
-        result.push(nums[i]);     // xᵢ
-        result.push(nums[i + n]); // yᵢ
+        result.push(nums[i]);
+        result.push(nums[i + n]);
     }
     return result;
-};
-console.log(shuffle([2,5,1,3,4,7], 3)); // [2,3,5,4,1,7]
-console.log(shuffle([1,2,3,4,4,3,2,1], 4)); // [1,4,2,3,3,2,4,1]
-console.log(shuffle([1,1,2,2], 2)); // [1,2,1,2]
+}
 
-
+// Example usage :
+console.log(shuffle([2,5,1,3,4,7], 3)); 
+console.log(shuffle([1,2,3,4,4,3,2,1], 4)); 
+console.log(shuffle([1,1,2,2], 2)); 
